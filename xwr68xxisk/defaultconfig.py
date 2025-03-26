30ecf650535f98fa15bbf4431ef3424e6d774dc3
Juergen Hasch <juergen.hasch@mmwaves.de>
2025-03-26 14:29:44 +0100

    Works

--- xwr68xxisk/defaultconfig.py
+++ xwr68xxisk/defaultconfig.py
@@ -4,7 +4,7 @@ This module contains predefined radar configurations for different hardware.
 """
 
 # Default frame period in milliseconds
-DEFAULT_FRAME_PERIOD = 400
+DEFAULT_FRAME_PERIOD = 100
 
 # Original hardcoded configurations as strings (kept for reference)
 awr2544_str = """
@@ -35,15 +35,15 @@ dfeDataOutputMode 1
 channelCfg 15 5 0
 adcCfg 2 1
 adcbufCfg -1 0 1 1 1
-profileCfg 0 60 567 7 57.14 0 0 35 1 256 5209 0 0 158
+profileCfg 0 60 567 7 57.14 0 0 70 1 256 5209 0 0 158
 chirpCfg 0 0 0 0 0 0 0 1
 chirpCfg 1 1 0 0 0 0 0 4
 frameCfg 0 1 16 0 {DEFAULT_FRAME_PERIOD} 1 0
 lowPower 0 0
-guiMonitor -1 1 0 0 0 1 1
+guiMonitor -1 1 0 0 0 0 1
 cfarCfg -1 0 2 8 4 3 0 10 1
 cfarCfg -1 1 0 4 2 3 1 10 1
-multiObjBeamForming -1 1 0.88
+multiObjBeamForming -1 1 0.9
 clutterRemoval -1 0
 calibDcRangeSig -1 0 -5 8 256
 extendedMaxVelocity -1 0
